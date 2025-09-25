546a1e8b24234ea8d218d9ab769ce48774d6988c
GADDIEL.TEVES <16101231@usc.edu.ph>
2025-09-25 16:12:35 +0800

    feat: add clerk auth to admin app

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,15 +1,19 @@
-import './assets/main.css'
-
-import { ApolloProvider } from '@apollo/client/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import client from './utils/apolloClient'
+import './assets/main.css'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={client}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
-    </ApolloProvider>
+    </ClerkProvider>
   </StrictMode>
 )
